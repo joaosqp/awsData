2995ec67d6a339ef9cd8915ba280b7a39567efdc
joaosqp <joaodesoouza@gmail.com>
2023-08-11 10:03:44 -0400

    Resolvido alguns exercicios de biblioteca

---
+++ Sprint 2/exercicios/exerciciosBiblioteca/exercicio3.sql
@@ -0,0 +1,4 @@
+/*E3
+ Apresente a query para listar as 5 editoras com mais livros na biblioteca. 
+ O resultado deve conter apenas as colunas quantidade, nome, estado e cidade. 
+ Ordenar as linhas pela coluna que representa a quantidade de livros em ordem decrescente.*/
