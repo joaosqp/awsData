7ac000ae77c9a901975d9c5c07dc995ace36c28b
joaosqp <joaodesoouza@gmail.com>
2023-08-14 08:31:59 -0400

    Terminado exercicios

--- Sprint 2/exercicios/exerciciosBiblioteca/exercicio4.sql
+++ Sprint 2/exercicios/exerciciosBiblioteca/exercicio4.sql
@@ -3,15 +3,14 @@ Apresente a query para listar a quantidade de livros publicada por cada autor.
 Ordenar as linhas pela coluna nome (autor), em ordem crescente. 
 Além desta, apresentar as colunas codautor, nascimento e quantidade (total de livros de sua autoria).*/
 
-select distinct
-    nome,
-    codautor,
-    nascimento,
+select 
+    aut.nome,
+    aut.codautor,
+    aut.nascimento,
     count(*) as quantidade 
     
 
 from autor as aut
 left join livro as liv
     on aut.codautor = liv.autor
-group by codautor
-order by nome
+group by aut.nome
