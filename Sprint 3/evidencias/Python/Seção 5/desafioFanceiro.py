1727410ebce5ea27c05738bd22e95872c4acc157
joaosqp <joaodesoouza@gmail.com>
2023-08-16 10:28:19 -0400

    Atualizado aulas

---
+++ Sprint 3/evidencias/Python/Seção 5/desafioFanceiro.py
@@ -0,0 +1,5 @@
+# Quantos % sobre do salario
+salario = 3450.45
+despesas = 2456.20
+
+print('salario comprometido', despesas / salario * 100)
---
+++ Sprint 3/evidencias/Python/Seção 5/fundamentos.py
@@ -0,0 +1,70 @@
+## primeiros exemplos
+
+# python não precisa de ; 
+
+print('Primeiro exemplo')
+
+
+## tipos basicos
+print(1+2)
+
+# Este é um comentário de linha única
+codigo = 42  # Isso é uma atribuição com um comentário no final
+
+
+"""
+Este é um comentário
+de múltiplas linhas.
+Pode ser usado para documentar funções ou explicar blocos de código extensos.
+"""
+
+'''
+Este é um comentário
+de múltiplas linhas.
+Pode ser usado para documentar funções ou explicar blocos de código extensos.
+'''
+
+
+
+
+# Adição (+): Usado para somar dois valores.
+a = 5
+b = 3
+resultado = a + b
+print(resultado)
+
+# Subtração (-): Usado para subtrair um valor de outro.
+a = 10
+b = 4
+resultado = a - b
+print(resultado) 
+
+# Multiplicação (*): Usado para multiplicar dois valores.
+a = 7
+b = 2
+resultado = a * b
+print(resultado)  
+
+# Divisão (/): Usado para dividir um valor pelo outro (geralmente resulta em um número de ponto flutuante).
+a = 15
+b = 3
+resultado = a / b
+print(resultado) 
+
+# Divisão inteira (//): Realiza a divisão e arredonda o resultado para baixo para o número inteiro mais próximo.
+a = 15
+b = 4
+resultado = a // b
+print(resultado) 
+
+# Módulo (%): Retorna o resto da divisão entre dois valores.
+a = 17
+b = 5
+resultado = a % b
+print(resultado)  # Output: 2
+
+# Potência (**): Calcula a potência de um número elevado a outro.
+a = 2
+b = 3
+resultado = a ** b
+print(resultado)
