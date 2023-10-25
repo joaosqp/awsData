52132683bc5a33b8336b741d2631d2f6c8d1915b
joaosqp <joaodesoouza@gmail.com>
2023-10-25 10:02:46 -0400

    Atualizado desafio1

---
+++ Sprint 8/exercicios/exercicio1/testeAPI.py
@@ -0,0 +1,32 @@
+import requests
+import pandas as pd
+
+from IPython.display import display
+
+
+api_key = "429396332743d52589579ea586d4057a"
+
+url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={api_key}&language=pt-BR"
+
+
+response = requests.get(url)
+data = response.json()
+
+
+filmes = []
+
+
+for movie in data['results']:
+      df = {'Titulo': movie['title'],
+            'Data de lançamento': movie['release_date'],
+            'Visão geral': movie['overview'],
+            'Votos': movie['vote_count'],
+            'Média de votos:': movie['vote_average']
+}
+
+
+filmes.append(df)
+
+
+df = pd.DataFrame(filmes)
+display(df)
