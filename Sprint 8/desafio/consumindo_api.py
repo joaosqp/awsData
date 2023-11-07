0cdbc86e5987ab9971d4d8c293a327a19a411e28
joaosqp <joaodesoouza@gmail.com>
2023-11-07 10:02:37 -0400

    Atualizado exercicios

--- Sprint 8/desafio/consumindo_api.py
+++ Sprint 8/desafio/consumindo_api.py
@@ -3,72 +3,78 @@ import boto3
 import os
 import requests
 import json
+from datetime import date
 
-# API e configurações
-chave_acesso_aws = 'AKIARTMJ3STNRAXPYP6H'
-chave_secreta_aws = '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'
-chave_api = "429396332743d52589579ea586d4057a"
-bucket = 'data-lake-do-joaosqp-filmes'
-caminho_s3 = 'data-lake-do-joaosqp-filmes/Raw/Local/CSV/Movies/2023/10/23/movies.csv'
+# Função para obter informações do filme da API TMDB
+def obter_informacoes_filme(movie_id, api_key):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}"
+    resposta = requests.get(url)
+    
+    if resposta.status_code == 200:
+        data = resposta.json()
+        orcamento = data.get('budget')
+        receita = data.get('revenue')
+        diretores = ', '.join([d['name'] for d in data.get('directors', [])])
+        atores_principais = ', '.join([a['name'] for a in data.get('cast', [])])
+        
+        return {
+            'ID': movie_id,
+            'Orçamento': orcamento,
+            'Receita': receita,
+            'Diretores': diretores,
+            'Atores Principais': atores_principais
+        }
+    else:
+        return None
 
-# Configuração das credenciais da AWS
-os.environ['AWS_ACCESS_KEY_ID'] = chave_acesso_aws
-os.environ['AWS_SECRET_ACCESS_KEY'] = chave_secreta_aws
-base_url = "https://api.themoviedb.org/3"
+def lambda_handler(event, context):
+    # Sua chave da API TMDB
+    api_key = "429396332743d52589579ea586d4057a"
 
-# Defina as configurações e variáveis globais aqui, como você já fez
+    # Sua chave de acesso da AWS
+    chave_acesso_aws = 'AKIARTMJ3STNRAXPYP6H'
+    chave_secreta_aws = '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'
 
-def obter_informacoes_adicionais_filme(filme_id):
-    orcamento = "Informação não disponível"
-    receita = "Informação não disponível"
+    bucket = 'data-lake-do-joaosqp-filmes'
+    caminho_s3 = 'data-lake-do-joaosqp-filmes/Raw/Local/CSV/Movies/2023/10/23/movies.csv'
 
-    url_creditos = f"{base_url}/movie/{filme_id}/credits"
-    parametros = {"api_key": chave_api}
-    data_creditos = requests.get(url_creditos, params=parametros).json()
+    s3_client = boto3.client('s3', aws_access_key_id=chave_acesso_aws, aws_secret_access_key=chave_secreta_aws)
+    objeto = s3_client.get_object(Bucket=bucket, Key=caminho_s3)
+    df = pd.read_csv(objeto['Body'], sep="|")
 
-    diretores = [membro["name"] for membro in data_creditos["crew"] if membro["job"] == "Director"]
-    atores_principais = [ator["name"] for ator in data_creditos["cast"] if ator["order"] in (0, 1, 2)]
+    # Filtrar filmes de comédia
+    comedy_movies = df['genero'].str.contains("Comedy")
 
-    return orcamento, receita, diretores, atores_principais
+    lista_filmes = []
 
-# LEITURA CSV
-s3_client = boto3.client('s3')
-objeto = s3_client.get_object(Bucket=bucket, Key=caminho_s3)
-df = pd.read_csv(objeto['Body'], sep="|")
+    for movie_id in comedy_movies['id']:
+        info_filme = obter_informacoes_filme(movie_id, api_key)
+        if info_filme:
+            lista_filmes.append(info_filme)
 
-# Filtrar filmes de comédia
-comedy_movies = df[df['genero'] == 'comedy']
+    base_path = 'data-lake-do-joaosqp-filmes/'
+    minha_camada = 'RAW'
+    minha_origem = 'TMDB'
+    meu_formato = 'JSON'
 
-# Iterar sobre os filmes de comédia e obter informações
-lista_filmes = []
+    data_de_processamento = date.today().strftime('%Y/%m/%d')
+    
+    # Dividir os dados em lotes de 100 registros
+    lotes = [lista_filmes[i:i + 100] for i in range(0, len(lista_filmes), 100)]
 
-for _, row in comedy_movies.iterrows():
-    movie_id = row['id']
-    orcamento, receita, diretores, atores_principais = obter_informacoes_adicionais_filme(movie_id)
+    # Gravar os lotes de dados em arquivos JSON no Amazon S3
+    for i, lote in enumerate(lotes):
+        arquivo_json = f'lote_{i}.json'
+        path_s3 = f"{base_path}{minha_camada}/{minha_origem}/{meu_formato}/{data_de_processamento}/{arquivo_json}"
+        
+        with open(arquivo_json, 'w') as json_file:
+            json.dump(lote, json_file)
 
-    # Adicionar informações do filme à lista de filmes
-    lista_filmes.append({
-        'ID': movie_id,
-        'Orçamento': orcamento,
-        'Receita': receita,
-        'Diretores': diretores,
-        'Atores Principais': atores_principais
-    })
+        # Enviar o arquivo JSON para o Amazon S3
+        s3 = boto3.client('s3', aws_access_key_id=chave_acesso_aws, aws_secret_access_key=chave_secreta_aws)
+        s3.upload_file(arquivo_json, bucket, path_s3)
 
-# Dividir os dados em lotes de 100 registros
-lotes = [lista_filmes[i:i + 100] for i in range(0, len(lista_filmes), 100)]
-
-# Gravar os lotes de dados em arquivos JSON no Amazon S3
-for i, lote in enumerate(lotes):
-    arquivo_json = f'lote_{i}.json'
-    path_s3 = f's3://{bucket}/data-lake-do-joaosqp-filmes/Tmdb/Json/2023/10/01/{arquivo_json}'
-
-    with open(arquivo_json, 'w') as json_file:
-        json.dump(lote, json_file)
-
-    # Enviar o arquivo JSON para o Amazon S3
-    s3 = boto3.client('s3')
-    s3.upload_file(arquivo_json, bucket, path_s3)
-
-# Exibir um print quando a operação for concluída
-print("Operação de envio para o Amazon S3 concluída.")
+    return {
+        'status code': 200,
+        'body': json.dumps(f"Funcionou")
+    }
