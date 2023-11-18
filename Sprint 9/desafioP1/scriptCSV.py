315dee2b9cd94b3ae79850fbbc6a078a2bbd520a
joaosqp <joaodesoouza@gmail.com>
2023-11-18 10:30:20 -0400

    Finalizado parte 1

--- Sprint 9/desafioP1/scriptCSV.py
+++ Sprint 9/desafioP1/scriptCSV.py
@@ -4,8 +4,8 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from pyspark.sql.functions import *
 
+## @params: [JOB_NAME, S3_INPUT_PATH, S3_TARGET_PATH]
 args = getResolvedOptions(sys.argv, ['JOB_NAME', 'S3_INPUT_PATH', 'S3_TARGET_PATH'])
 
 sc = SparkContext()
@@ -19,5 +19,6 @@ target_path = args['S3_TARGET_PATH']
 
 df = spark.read.format("csv").option("header", "true").load(source_file)
 
-df_repartitioned = df.repartition(1)  # Reduz para uma única partição
-df_repartitioned.write.mode("append").parquet(target_path)
+df.write.mode("append").parquet(target_path)
+
+job.commit()
--- Sprint 9/desafioP1/scriptJSON.py
+++ Sprint 9/desafioP1/scriptJSON.py
@@ -1,11 +1,11 @@
 import sys
-from awsglue.transforms import *
+import pandas as pd
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from pyspark.sql.functions import *
 
+## @params: [JOB_NAME, S3_INPUT_PATH, S3_TARGET_PATH]
 args = getResolvedOptions(sys.argv, ['JOB_NAME', 'S3_INPUT_PATH', 'S3_TARGET_PATH'])
 
 sc = SparkContext()
@@ -17,15 +17,12 @@ job.init(args['JOB_NAME'], args)
 source_path = args['S3_INPUT_PATH']
 target_path = args['S3_TARGET_PATH']
 
-# Extrair a data da estrutura do caminho de origem
-ano = "2023"
-mes = "11"
-dia = "07"
+df = spark.createDataFrame(pd.read_json(f"{source_path}lote_0.json"))
 
-# Leitura dos arquivos JSON
-df = spark.read.json(source_path)
+for i in range(1, 7):
+    df_for = spark.createDataFrame(pd.read_json(f"{source_path}lote_{i}.json"))
+    df = df.union(df_for)
 
-# Escrever os dados no formato Parquet com o nome do arquivo contendo a data
-df.write.mode("append").parquet(f"{target_path}/{ano}/{mes}/{dia}")
+df.write.mode("append").parquet(target_path)
 
-
+job.commit()
